d8ea6e681aced26c1caa39f39f194b39aace351b
comaldave <skinner.david@gmail.com>
2021-04-02 15:23:09 -0500

    Updated doc.go

--- doc.go
+++ doc.go
@@ -1,6 +1,25 @@
-/*
-Package frodo implements Foundational Recursive Orthogonal Development Orders in a wise way.
+// file 'pkg.davsk.net/frodo/doc.go
 
+// by David Lynn Skinner
+// on April 2, 2021
+// for Davsk Ltd Co
+
+// Copyright 2021 Davsk Ltd Co. All rights reserved.
+// Use of this source code is governed by an MIT style license that can be found
+// in the LICENSE file.
 
 /*
-package frodo
+Package frodo implements Foundational Recursive Orthogonal Development Orders in
+a wise way for Davsk Ltd Co CASE tools. First run acquires user info, verifies
+dev env and installs CASE tools. Program may be run again to perform specific
+actions.
+
+OVERVIEW
+
+Frodo may be deployed on any platform as a CLI or GUI app. It is used as a
+wrapper for a variety of CASE tools. It is very much a dynamic work in progress
+and should not be considered stable at this time.
+
+BUG(david@davsk.net): This is an ALPHA package under development.
+*/
+package frodo // import pkg.davsk.net/frodo
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/YOUR-USER-OR-ORG-NAME/YOUR-REPO-NAME
+module pkg.davsk.net/frodo
 
-go 1.14
+go 1.16
