94be53fb0ff62e4fe73b6441cedc9f77a0422089
David Skinner <skinner.david@gmail.com>
2025-01-07 17:15:34 -0600

    Update frodo.go
    select by OS and Arch, prototyping.

--- frodo.go
+++ frodo.go
@@ -11,15 +11,39 @@ isDarwin := false
 isLinux := false
 isWindows := false
 isUnknown := false
+isAmd64 := false
 
 func main() {
-  switch ( runtime.GOOS )
-  case "darwin"
-    isDarwin = true
-  case "linux"
-   isLinux = true
-  case "windows"
-    isWindows = true
-  default
-    isUnknown = true
+  switch ( runtime.GOOS ) {
+  case "darwin":
+    DarwinMustDoit( runtime.GOARCH )
+  case "linux":
+    LinuxMustDoit( runtime.GOARCH )
+  case "windows":
+    WindowsMustDoit( runtime.GOARCH )
+  default:
+    ReportUnknown( runtime.GOOS, runtime.GOARCH )
 }
+  )
+
+func DarwinMustDoit(arch) { 
+  //
+}  
+
+ func LinuxMustDoit(arch) {
+   if ( arch != "Amd86" ) {
+     ReportUnknown( runtime.GOOS, runtime,GOARCH )
+   } else if ( // test for pacman ) {
+   }
+  
+  switch ( runtime.GOARCH ) {
+    case "amd64" :
+    isAmd64 = true
+    default:
+    isAmd64 = false
+    }
+
+  if ( isDarwin ) {
+    Darwin.mustDoit()
+    } else if ( isLinux 
+  }
