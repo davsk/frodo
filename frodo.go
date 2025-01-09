6779f1100e6bf21ebc8872e94411b5074b0a87a9
David Skinner <skinner.david@gmail.com>
2025-01-09 08:11:52 -0600

    fix(release): More corrections.

--- frodo.go
+++ frodo.go
@@ -1,41 +1,38 @@
 // 'frodo.go'
 
 // Package frodo automates updates.
-package frodo // import "go.davsk.net/frodo"
+package main // import "go.davsk.net/frodo"
 
 import (
-  runtime
+	"runtime"
 
-  go.davsk.net/frodo/pkg/auto_update
-
+	"frodo/pkg/auto_update"
 )
 
 func main() {
-
-	auto_update.MustDoit("frodo","github.com/davsk/frodo","v0.0.1")
-
-switch ( runtime.GOOS ) {
-  case "darwin":
-    DarwinMustDoit( runtime.GOARCH )
-  case "linux":
-    LinuxMustDoit( runtime.GOARCH )
-  case "windows":
-    WindowsMustDoit( runtime.GOARCH )
-  default:
-    ReportUnknown( runtime.GOOS, runtime.GOARCH )
+	auto_update.MustDoit("frodo", "github.com/davsk/frodo", "v0.0.1")
 }
-  )
 
-func DarwinMustDoit(arch) { 
-  //
-}  
+// switch ( runtime.GOOS ) {
+//   case "darwin":
+//     DarwinMustDoit( runtime.GOARCH )
+//   case "linux":
+//     LinuxMustDoit( runtime.GOARCH )
+//   case "windows":
+//     WindowsMustDoit( runtime.GOARCH )
+//   default:
+//     ReportUnknown( runtime.GOOS, runtime.GOARCH )
+// }
 
- func LinuxMustDoit(arch) {
-   if ( arch != "Amd86" ) {
-     ReportUnknown( runtime.GOOS, runtime,GOARCH )
-   } else if ( not pacman ) {
-      ReportError("Not Arch Linux"
-                  } else {
-                  ArchMustDoit()
-   }
-  }
+// func DarwinMustDoit(arch) {
+//   //
+// }
+//
+// func LinuxMustDoit(arch) {
+//    if ( arch != "Amd86" ) {
+//      ReportUnknown( runtime.GOOS, runtime,GOARCH )
+//    } else if ( not pacman ) {
+//       ReportError("Not Arch Linux"
+//                   } else {
+//                   ArchMustDoit()
+//    }
