8ef13ca41a292dd23c4f3220a635890e847ef092
David Skinner <skinner.david@gmail.com>
2025-01-10 21:01:05 -0600

    add install

--- cmd/frodo/frodo.go
+++ cmd/frodo/frodo.go
@@ -5,9 +5,11 @@ package main
 import (
 	"go.davsk.net/frodo/pkg/auto_update"
 	"go.davsk.net/frodo/pkg/config"
+	"go.davsk.net/frodo/pkg/install"
 )
 
 func main() {
+	install.hashicorp()
 	auto_update.MustDoit("frodo", "github.com/davsk/frodo", "v0.0.1")
 	config.Load()
 }
