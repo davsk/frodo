50788ce2d7dc7b433e5c5f02385b6907b79364aa
David Skinner <skinner.david@gmail.com>
2025-01-16 22:41:52 -0600

    Update privilege_windows.go

--- pkg/privilege/privilege_windows.go
+++ pkg/privilege/privilege_windows.go
@@ -12,3 +12,37 @@
 // and restarts with the requested privilege
 
 package privilege
+
+import (
+   	"syscall"
+   	"time"
+   	"os"
+   	"strings"
+   	"fmt"
+   	"golang.org/x/sys/windows"
+)
+
+func becomeAdmin() {
+   	verb := "runas"
+   	exe, _ := os.Executable()
+   	cwd, _ := os.Getwd()
+   	args := strings.Join(os.Args[1:], " ")
+
+   	verbPtr, _ := syscall.UTF16PtrFromString(verb)
+   	exePtr, _ := syscall.UTF16PtrFromString(exe)
+   	cwdPtr, _ := syscall.UTF16PtrFromString(cwd)
+   	argPtr, _ := syscall.UTF16PtrFromString(args)
+
+   	var showCmd int32: 1 //SW_NORMAL
+
+   	err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
+   	if err != nil {
+  		fmt.Println(err)
+   	}
+}
+
+func checkAdmin() bool {
+   	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
+
+    return err == nil
+}
