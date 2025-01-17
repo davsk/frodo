79a6f0da01e398dcc06d848ef1b8a7d72113c0c3
David Skinner <skinner.david@gmail.com>
2025-01-16 19:36:46 -0600

    Create install_linux.go

---
+++ pkg/install/install_linux.go
@@ -0,0 +1,10 @@
+// +build linux
+// file: 'frodo/pkg/install/install_linux.go'
+
+// by David Lynn Skinner
+// on January 16, 2025
+// for Davsk Ltd Co
+
+// copyright (c) 2025 Davsk Ltd Co
+
+package install
