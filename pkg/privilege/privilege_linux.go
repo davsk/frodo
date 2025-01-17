562dfaaa6ed385a480b3527008ce4cd904e1c69d
David Skinner <skinner.david@gmail.com>
2025-01-16 22:39:51 -0600

    Create privilege_linux.go

---
+++ pkg/privilege/privilege_linux.go
@@ -0,0 +1,14 @@
+// +build linux
+// file: 'frodo/pkg/privilege/privilege_linux.go'
+
+
+// by David Lynn Skinner
+// on January 10, 2025
+// for Davsk Ltd Co
+
+// copyright (c) 2025 Davsk Ltd Co
+
+// package privilege checks for root, sudo, or admin privilege
+// and restarts with the requested privilege
+
+package privilege
