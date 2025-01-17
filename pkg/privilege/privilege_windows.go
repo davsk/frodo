dbba097e1650115bce27362db3a5041287f1531c
David Skinner <skinner.david@gmail.com>
2025-01-16 22:38:26 -0600

    Create privilege_windows.go

---
+++ pkg/privilege/privilege_windows.go
@@ -0,0 +1,14 @@
+// +build windows
+// file: 'frodo/pkg/privilege/privilege_windows.go'
+
+
+// by David Lynn Skinner
+// on January 16, 2025
+// for Davsk Ltd Co
+
+// copyright (c) 2025 Davsk Ltd Co
+
+// package privilege checks for root, sudo, or admin privilege
+// and restarts with the requested privilege
+
+package privilege
