91b6ce3e02df7dea7bdc940c9148ecb2962df520
David Skinner <skinner.david@gmail.com>
2025-03-15 11:50:49 -0500

    update

---
+++ main.go
@@ -0,0 +1,19 @@
+// file: "frodo/main.go"
+
+// by David Lynn Skinner
+// for Davsk Ltd Co
+// revisions Jan 2025
+
+// Copyright 2025 Davsk Ltd Co. All rights reserved.
+// Use of this source code is governed by an MIT style license that can be found
+// in the LICENSE file.
+
+// start frodo cli
+
+package main
+
+import "go.davsk.net/frodo/cmd"
+
+func main() {
+	cmd.Execute()
+}
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,14 +3,35 @@
 // Package config contains the init, edit, save, and load of the frodo config.
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"os/exec"
+)
 
+// type Profile contains values of user and run count.
 type Profile struct {
+	countRuns int
 	UserName, UserEmail string
 }
 
 // New
 func Load() *Profile {
 	fmt.Printf("test.")
+
+	// golang check
+	if _, err := exec.LookPath("go"); err == nil {
+		fmt.Println("Go is already installed.")
+		return nil
+	} else {
+		// install.Golang
+	}
+
+	// gh check
+	if _, err := exec.LookPath("gh"); err == nil {
+		fmt.Println("gh is already installed.")
+		return nil
+	} else {
+		// install.Golang
+	}
 	return new(Profile)
 }
